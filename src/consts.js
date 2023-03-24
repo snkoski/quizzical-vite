db61a60a323e61ee8918e0c9570d60958cd3ec12
Shawn Koski <shawnk@aplos.com>
2023-03-23 17:49:49 -0700

    add consts file

---
+++ src/consts.js
@@ -0,0 +1,15 @@
+export const categories = [
+  { value: "", label: "All Categories" },
+  { value: "9", label: "General Knowlege" },
+  { value: "10", label: "Books" },
+  { value: "17", label: "Science & Nature" },
+  { value: "18", label: "Science & Computers" },
+  { value: "19", label: "Mathematics" },
+  { value: "20", label: "Mythology" },
+  { value: "21", label: "Sports" },
+  { value: "22", label: "Geography" },
+  { value: "23", label: "History" },
+  { value: "24", label: "Politics" },
+  { value: "25", label: "Art" },
+  { value: "28", label: "Vehicles" },
+];
